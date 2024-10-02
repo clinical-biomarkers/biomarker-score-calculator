c4bc584c32f131abcf09104e0f19bc78ae14b2ef
Sean Kim <33474168+seankim658@users.noreply.github.com>
2024-10-02 11:03:02 -0400

    Fix individual vs list field handling

--- src/rules/engine.rs
+++ src/rules/engine.rs
@@ -26,21 +26,26 @@ fn evaluate_condition<B: BiomarkerData>(biomarker: &B, condition: &Condition) ->
             .iter()
             .filter(|e| e.database().to_lowercase() != "pubmed")
             .all(|e| e.database() == value),
-        Condition::FieldEquals { field, value } => match_field(biomarker, field) == *value,
+        Condition::FieldEquals { field, value } => {
+            match_field(biomarker, field).iter().all(|f| f == value)
+        }
         Condition::FieldAllContains { field, value } => {
-            match_field(biomarker, field).contains(value)
+            !match_field(biomarker, field).is_empty()
+                && match_field(biomarker, field)
+                    .iter()
+                    .all(|f| f.contains(value))
         }
         Condition::FieldSomeContains { field, value } => {
-            match_field(biomarker, field).contains(value)
+            match_field(biomarker, field).iter().any(|f| f.contains(value))
         }
         Condition::FieldLenGreaterThan { field, value } => {
-            match_field(biomarker, field).split(',').count() as f64 > *value
+            match_field(biomarker, field).len() as f64 > *value
         }
         Condition::FieldLenLessThan { field, value } => {
-            (match_field(biomarker, field).split(',').count() as f64) < *value
+            (match_field(biomarker, field).len() as f64) < *value
         }
         Condition::FieldLenEqual { field, value } => {
-            match_field(biomarker, field).split(',').count() as f64 == *value
+            match_field(biomarker, field).len() as f64 == *value
         }
         Condition::And { conditions } => {
             conditions.iter().all(|c| evaluate_condition(biomarker, c))
@@ -59,29 +64,26 @@ fn apply_action(score: f64, action: &Action) -> f64 {
     }
 }
 
-fn match_field<B: BiomarkerData>(biomarker: &B, field: &Field) -> String {
+fn match_field<B: BiomarkerData>(biomarker: &B, field: &Field) -> Vec<String> {
     match field {
-        Field::BiomarkerID => biomarker.biomarker_id().to_string(),
+        Field::BiomarkerID => vec![biomarker.biomarker_id().to_owned()],
         Field::ComponentEvidenceSourceDatabase => biomarker
             .biomarker_components()
             .iter()
             .flat_map(|c| c.evidence_source())
-            .map(|e| e.database())
-            .collect::<Vec<_>>()
-            .join(","),
-        Field::ConditionID => biomarker.condition_id().to_string(),
+            .map(|e| e.database().to_owned())
+            .collect(),
+        Field::ConditionID => vec![biomarker.condition_id().to_owned()],
         Field::TopEvidenceSourceDatabase => biomarker
             .evidence_sources()
             .iter()
-            .map(|e| e.database())
-            .collect::<Vec<_>>()
-            .join(","),
+            .map(|e| e.database().to_owned())
+            .collect(),
         Field::LoincCode => biomarker
             .biomarker_components()
             .iter()
             .flat_map(|s| s.specimen())
-            .map(|l| l.loinc_code())
-            .collect::<Vec<_>>()
-            .join(","),
+            .map(|l| l.loinc_code().to_owned())
+            .collect(),
     }
 }
